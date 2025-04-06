3a5cbef7cbf9da6e9fb1706a770f605fd47b72d1
revanthsai1997 <revanthsaivarma@gmail.com>
2025-04-06 15:17:51 -0500

    implemented redux

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello, world!");
+import { createStore } from "redux";
+
+const localState = {
+    count: 0,
+    actions:[]
+}
+
+function reducer(state=localState,action){
+    if(action.type == 'Increase'){
+        return {count:state.count+action.payload, actions:[...state.actions,action]};
+    }
+    else if(action.type == 'Decrease'){
+        return {count:state.count-action.payload, actions:[...state.actions,action]};
+    }
+    else if(action.type == 'Reset'){
+        return {count:0, actions:[...state.actions,action]};
+    }
+    return state;
+
+}
+
+const store = createStore(reducer);
+
+store.subscribe(()=>{
+    console.log(store.getState());
+})
+
+const Increase = (val)=>{return {type:'Increase',payload:val}}
+const Decrease = (val)=>{return {type:'Decrease',payload:val}};
+
+store.dispatch(Increase(100));
+store.dispatch(Increase(500));
+store.dispatch(Increase(1000));
+store.dispatch(Decrease(500));
+store.dispatch({type:''});
